48166399212e85813723a9130325890bfbd8b3a3
Yuechen Li <liyuechen@buaa.edu.cn>
2025-11-23 23:59:42 +0900

    update

--- batch_run.bat
+++
@@ -1,6 +0,0 @@
-python -m mycode.run --program comp --rq 4
-python -m mycode.run --program amplitude --rq 4
-python -m mycode.run --program pauli --rq 4
-python -m mycode.run --program qft --rq 4
-python -m mycode.run --program quad --rq 4
-python -m mycode.run --program adder --rq 4
